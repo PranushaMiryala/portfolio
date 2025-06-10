38858c58a9b52f5329f9b3a5e794a9b118f15670
Pranusha Reddy <pranushareddy925@gmail.com>
2025-06-10 13:11:06 +0530

    Initial portfolio commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import styles from "./About.module.css";
+import React from "react";
 
-export cdconst About = () => {
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
   return (
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
-      <div className={styles.aboutItemText}>
-        <p>
-          A passionate and driven engineering student with a strong foundation in full-stack development and real-world project experience.
-        </p>
-        <p>
-          Eager to contribute to innovative solutions through teamwork, creativity, and continuous learning.
-        </p>
-        <p>
-          Actively growing as a developer while aligning with industry best practices and future-ready technologies.
-        </p>
+      <div className={styles.content}>
+        
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            
+            <div className={styles.aboutItemText}>
+              
+              <p>
+                I'm a dedicated full-stack developer with experience in building modern, end-to-end applications using technologies like React, Node.js, Express.js, and MongoDB, as well as full-stack Java with Spring Boot. I also create data-driven tools and dashboards using Python and Streamlit, supported by secure databases like SQLite. With a strong foundation in frontend design, backend architecture, and database management, I enjoy solving complex problems with clean, scalable solutions. My approach combines technical precision, adaptability, and a passion for continuous learning to deliver impactful and reliable software.
+              </p>
+            </div>
+          </li>
+          
+          
+        </ul>
       </div>
     </section>
   );
 };
-
-
